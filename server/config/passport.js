f40162681225891b6329b43d1842f7714d4402f2
GEOkettle <jsonmiou@gmail.com>
2022-04-04 07:37:39 +0900

    middleware

--- server/config/passport.js
+++ server/config/passport.js
@@ -21,13 +21,17 @@ module.exports = passport => {
     // if the user is not found, call done with error and false
     
     passport.use(new JwtStrategy(opts, (jwt_payload, done) =>{
+     
         User.findById(jwt_payload.id)
             .then(user => {
                 if(user) {
-                    return done(null, user);
+                    return done(null, user)
                 }
+              
                 return done(null, false);
             })
-            .catch(err => console.log(err));
+            .catch(err => console.log(err),
+           
+            );
     }));
 };
--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
 require('dotenv').config(); //.env file is should be in root directory
-
+const { auth } = require("../../middleware/auth");
 
 const { JWT_ACCESS_SECRET, JWT_ACCESS_EXPIRATION_TIME,JWT_REFRESH_SECRET,JWT_REFRESH_EXPIRATION_TIME } = process.env;
 const bcrypt = require('bcrypt');
@@ -14,6 +14,10 @@ const res = require('express/lib/response');
 router.get('/', (req, res) => {
     console.log('router')
     res.send("패스포트 모듈 테스트");
+
+});
+
+router.get('/auth', (req, res) => {
 
 });
 router.post('/register', (req, res) => {
@@ -37,42 +41,47 @@ router.post('/login', (req, res) => {
             if (!user) {
                    return res.json({
                 loginSuccess: false,
-                message: "Auth failed, email not found"
+                message: "Auth failed, ID not found"
                 });
             }
         
     //compare password
     bcrypt.compare(password, user.password)
-                .then(isMatch => {
-                    if (isMatch) {
-                        const payload = {
-                            id: user.id,
-                            name: user.name,
-                            userid: user.userid,
-                            email: user.email,
-                            regi_date: user.register_date,
-                            role: user.role
-                        };
-                        const refreshpayload = {
-                            id: user.id
-                        }
-                        //generate Access token
-                        jwt.sign(payload, JWT_ACCESS_SECRET, { expiresIn: JWT_ACCESS_EXPIRATION_TIME }, (err, token) => {
-            
-                            //how do i console.log exp in jwt.sign
-                            res.cookie('jwt', token, {httpOnly :true, maxAge : JWT_ACCESS_EXPIRATION_TIME})
-                            res.json({
-                                success: true,
-                                accessToken: 'Bearer ' + token,
-                                
-                            })
-                        });
-                        // jwt.sign(refreshpayload, JWT_REFRESH_SECRET, { expiresIN: JWT_REFRESH_EXPIRATION_TIME }, (err, token) => {
-
-                        // });
-                    } else {
-                        return res.json({ loginSuccess: false, message: "Wrong password" });
-                    }
+        .then(isMatch => {
+            if (isMatch) {
+            const accessPayload = {
+                id: user.id,
+                name: user.name,
+                userid: user.userid,
+                email: user.email,
+                regi_date: user.register_date,
+                role: user.role
+            };
+            // console.log(accessPayload.id);
+            const refreshPayload = {
+                id: user.id
+            }
+            //generate Access token
+            jwt.sign(accessPayload, JWT_ACCESS_SECRET, { expiresIn:  JWT_ACCESS_EXPIRATION_TIME}, (err, token) => {
+                
+                //how do i console.log exp in jwt.sign
+                const accessToken = 'Bearer ' + token
+                const accessT = token
+               
+                jwt.sign(refreshPayload, JWT_REFRESH_SECRET, { expiresIn: JWT_REFRESH_EXPIRATION_TIME }, (err, token) => {
+                    //note that maxAge is in milliseconds
+                    res.cookie('accessToken', accessT, { httpOnly: true, maxAge: 1000 * 60 * 30 })//secure :true
+                    res.cookie('refreshToken', token, { httpOnly: true, maxAge: 1000 * 60 * 60 * 24 * 14 })
+                    res.json({
+                        success: true,
+                        refreshToken: 'Bearer ' + token,
+                        accessToken: accessToken
+                    })
+                });
+            });
+            } else {
+                return res.json({ loginSuccess: false, message: "Wrong password" });
+            }
             })
         })
 
