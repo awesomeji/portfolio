504e4aa4a22cec45e6b921559508144d2b664f78
GEOkettle <jsonmiou@gmail.com>
2022-04-22 02:47:25 +0900

    react-router got issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
-
-
+import {Route,Routes,BrowserRouter} from 'react-router-dom'
+import About from './components/About/About'
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      
-    </div>
-  )
+    const [count, setCount] = useState(0)
+    return (
+    
+        <Routes>
+            <Route path='/' element={ <Home />} />
+            <Route path='about' element={ <About />} />
+            </Routes>
+        // <About />
+        // <BrowserRouter>
+        //     <Switch>
+        //     <Route path='/' component={Home} />               
+        //     <Route path='/about' component={About} />               
+        //     </Switch>
+        // </BrowserRouter>
+        
+    )
 }
 
 export default App
