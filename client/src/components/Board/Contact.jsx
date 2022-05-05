6b31275250ab0740eb87a22863ece05722b413e1
GEOkettle <jsonmiou@gmail.com>
2022-05-06 00:26:50 +0900

    board almost done

--- client/src/components/Board/Contact.jsx
+++ client/src/components/Board/Contact.jsx
@@ -5,47 +5,139 @@ import useStore from '../../store/store';
 import {Link} from 'react-router-dom';
 
 export default function ContactMe() {
-    const { loginStatus, accessToken } = useStore();
-    
-   
+    const { loginStatus} = useStore();
+    const [totalPage, setTotalPage] = useState(0);
+    const [page, setPage] = useState(1);
+    const [perPage, setPerPage] = useState(10);
+    const [data, setData] = useState();
+    const [total, setTotal] = useState();
+    const [sortBy, setSortBy] = useState('createdAt');
+    const [sortOrder, setSortOrder] = useState(-1);
+    const [searchOptionA, setSearchOptionA] = useState('');
+    const [searchOptionB, setSearchOptionB] = useState('');
+    const [searchOptions, setSearchOptions] = useState([]);
+
+
+
 
     useEffect(() => { 
-        axios.get('/api/board/list')
+
+        const request = {
+            page: page,
+            perPage: perPage,
+            sortBy: sortBy,
+            sortOrder: sortOrder,
+            searchOptionA: searchOptions[0],
+            searchOptionB: searchOptions[1]
+        }
+        axios.get('/api/board/list',{params:request})
             .then(res => { 
-                console.log(res.data)
+                console.log(res)
                 if (!res.data.success) { 
                     alert(res.data.error)
                 } else {
-                    console.log(res.data.posts)
+                    
+                  
+                    setData(res.data.posts);
+                    setTotalPage(res.data.totalPage);
+                    setTotal(res.data.total);
                 }
-
+                
             })
-    }, [])
+        }, [page,perPage,sortBy,sortOrder,searchOptions])
+        
+       
+        let pageNumber = [];
+        for (let i = 1; i <= totalPage; i++) { 
+            pageNumber.push(i);
+        }
+    const onPerPageHandler = (e) => { 
+        e.preventDefault();
+        setPerPage(e.target.value);
+        setPage(1);
+    }
+
+    const onSearchOptionAHandler = (e) => { 
+        e.preventDefault();
+        setSearchOptionA(e.target.value);
+    }
+    const onSearchOptionsHandler = (e) => {
+        e.preventDefault()
+        setSearchOptions([searchOptionA, searchOptionB])
+    }
+    console.log(searchOptions)
+
+    const setSortHandlaer = (e) => { 
+        console.log(e);
+        setSortBy(e);
+        sortOrder===1 ? setSortOrder(-1) : setSortOrder(1); 
+    }
+    let i = 0;
+    
     return (
         <>
-            {loginStatus ? ( <div>
+            {loginStatus ? (
+                <div>
+                    <form onSubmit={e=>onSearchOptionsHandler(e)}>
+                        <select value={searchOptionA} onChange={onSearchOptionAHandler}>
+                            <option value="">선택</option>
+                            <option value="title">제목</option>
+                            <option value="writer">아이디</option>
+                        </select>
+                        <input value={searchOptionB} onChange={(e)=> setSearchOptionB(e.target.value) }  type="text" />
+                        <button type="submit">검색</button>
+                    </form>
                 <table border="1">
                     <colgroup>
                     </colgroup>
                     <thead>
                         <tr>
-                            <th>list</th>
-                            <th>아</th>
-                            <th>거대충</th>
+                            <th>번호</th>
+                                <th> <span onClick={()=>setSortHandlaer('title') }>제목</span> </th>
+                                <th> <span onClick={()=>setSortHandlaer('writer') }>작성자</span> </th>
+                                <th> <span onClick={()=>setSortHandlaer('createdAt') }>작성날짜</span> </th>
                      </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>list</td>
-                            <td>list테이블로있다치자고</td>
-                            <td>write랑 view 먼저만들고 해보자고</td>
-                        </tr>
+                            {data && data.map((item, index) => {
+                                const realtime = new Date(item.createdAt).toLocaleString();
+                                i++;
+                               
+                               
+                                return (
+                                    <tr key={i}>
+                                        <td>{i}</td>
+                                        <td>{item.title}</td>
+                                        <td>{item.writer.userid}</td>
+                                        <td>{realtime}</td>
+                                    </tr>
+                                )
+                            })}
                     </tbody>
-                </table>
+                    </table>
+                    <li >
+                        <span key={page!==1? page-1 : 0 } onClick={() => { if (page !== 1) { setPage(page - 1) } }}>◀</span>
+                    {pageNumber.map((page) => (
+                        <span key={page} onClick={() => setPage(page)}>{page}&nbsp;</span>
+                        ))}
+                        <span key={page!==totalPage? page+1 : totalPage+1} onClick={() => { if (page !== totalPage) { setPage(page + 1) } } }>▶</span>
+                        
+                            </li>
+                    
+                        <select value={perPage} onChange={onPerPageHandler}>
+                            <option value="10">10개씩보기</option>
+                            <option value="15">15개씩보기</option>
+                            <option value="20">20개씩보기</option>
+                            <option value="30">30개씩보기</option>
+                        </select>
+                   
+            
+                    <div>
+
+              
+                    </div>
                     <Link to="/write">글쓰기</Link>
-                <button >
-                    페찌
-                    </button>
+                
             </div>
             ) : (<></>)}
      
--- server/routes/api/board.js
+++ server/routes/api/board.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const { Board } = require('../../models/Board');
-
+const { User } = require('../../models/User');
 router.post('/write', (req, res) => { 
     const post = new Board(req.body);
 
@@ -16,12 +16,40 @@ router.post('/write', (req, res) => {
 })
 
 
-router.get('/list', (req, res) => { 
-    Board.find()
+router.get('/list', async (req, res) => { 
+    console.log(req.query)
+    const page = Number(req.query.page || 1); // 값이 없다면 기본값으로 1 사용
+    const perPage = Number(req.query.perPage || 10);
+    const SortBy = req.query.sortBy || 'createdAt';
+    const SortOrder = req.query.sortOrder || -1;
+    const conditionA = req.query.searchOptionA || '';
+    const conditionB = req.query.searchOptionB || '';
+    const total = await Board.countDocuments();
+    let query = {};
+   
+    if (conditionA === 'title') {
+        query = { 'title': conditionB }
+    } else if (conditionA === 'writer') { 
+        let id;
+        await User.find({ 'userid': conditionB })
+            .then(user => { 
+                id = user[0]._id;
+            })
+        query = { 'writer': id }
+    }
+    console.log(query)
+   
+    Board.find(query)
+        .populate('writer')
+        .sort([[SortBy, SortOrder]])
+        .skip((page - 1) * perPage)
+        .limit(perPage)
         .then(posts => { 
             return res.status(200).json({
                 success: true,
-                posts: posts
+                posts: posts,
+                total: total,
+                totalPage: Math.ceil(total / perPage)
             })
         })
         .catch((err) => { 
