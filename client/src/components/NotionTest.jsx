96cc0f799914dc9f806a93649a6c6fa9387d9914
GEOkettle <jsonmiou@gmail.com>
2022-05-10 17:57:09 +0900

    notynoty

--- client/src/components/NotionTest.jsx
+++ client/src/components/NotionTest.jsx
@@ -15,7 +15,7 @@ export default function About() {
     const [response, setResponse] = useState({})
       useEffect(() => {
     // 두번째 방법
-    const NOTION_PAGE_ID = '83fca179f8314fd784e541e3368df6a5';
+    const NOTION_PAGE_ID = 'c2d5f38d25c640a9a57e41ebfd2fcb7f?v=670f59c16fd2471fa0065e0f5f2e9437';
     fetch(`https://notion-api.splitbee.io/v1/page/${NOTION_PAGE_ID}`)
       .then(res => res.json())
       .then((resJson) => {
