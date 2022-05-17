c3551440b801132a6bf55158c389820c4780389a
GEOkettle <jsonmiou@gmail.com>
2022-05-18 00:49:19 +0900

    got it

--- client/src/components/Notion/Notion.jsx
+++ client/src/components/Notion/Notion.jsx
@@ -57,7 +57,7 @@ export default function Notion() {
               />
           </MainFrame>
           <GoToSuggestion>
-              test 
+              
                 <StyledLink to="/board">go To suggestion</StyledLink>
           </GoToSuggestion>
     </ThemeProvider>
